35d9d22d5a95fce4189dfb75a66fcb0cd5c4683e
luk3Sky <ganindudananja@gmail.com>
2020-05-22 14:40:58 +0530

    Updates

---
+++ precache-manifest.c03b524edc0b05b3e42115771646355c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2426d2e766b281d292efb662392c4e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65eef3bc27e93a131c98",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bf0e6f0aaa3ab4bc00a",
+    "url": "/static/js/2.b7eae52d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7eae52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65eef3bc27e93a131c98",
+    "url": "/static/js/main.16368d20.chunk.js"
+  },
+  {
+    "revision": "9bd4a39e68e57b7e8617",
+    "url": "/static/js/runtime-main.3d643903.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
